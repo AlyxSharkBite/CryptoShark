3275737333e006f96b51cc850e2c01ac50d70126
Alyx SharkBite <AlyxSharkBite@outlook.com>
2020-10-20 13:27:03 -0700

    Added Missing File not sure how it got left out.

---
+++ Interfaces/IHash.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace CryptoShark.Interfaces
+{
+    /// <summary>
+    ///     Hash Interface
+    /// </summary>
+    public interface IHash
+    {
+        /// <summary>
+        ///     Hashes data
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="encoding"></param>
+        /// <param name="hashAlgorithm"></param>
+        /// <returns></returns>
+        string Hash(ReadOnlySpan<byte> data, StringEncoding encoding, HashAlgorithm hashAlgorithm);
+
+        /// <summary>
+        ///     Hashes Data 
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="hashAlgorithm"></param>
+        /// <returns></returns>
+        ReadOnlySpan<byte> Hash(ReadOnlySpan<byte> data, HashAlgorithm hashAlgorithm);
+    }
+}
