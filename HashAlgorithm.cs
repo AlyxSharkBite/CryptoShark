cb5b32291b2584bf74e484d21f2df52c8a345bfd
Alyx SharkBite <AlyxSharkBite@outlook.com>
2020-10-20 13:30:11 -0700

    More missing fiels...  Starting to think VS for MacOS has a couple bugs.

---
+++ HashAlgorithm.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace CryptoShark
+{
+    /// <summary>
+    ///     Hashing Algorithms
+    /// </summary>
+    public enum HashAlgorithm
+    {
+        /// <summary>
+        ///     MD5 128 Bit
+        ///     https://en.wikipedia.org/wiki/MD5
+        /// </summary>
+        MD5,
+
+        /// <summary>
+        ///     SHA-1 160 Bit
+        ///     https://en.wikipedia.org/wiki/SHA-1
+        /// </summary>
+        SHA1,
+
+        /// <summary>
+        ///     SHA-2 256 Bit
+        ///     https://en.wikipedia.org/wiki/SHA-2
+        /// </summary>
+        SHA2_256,
+
+        /// <summary>
+        ///     SHA-2 384 Bit
+        ///     https://en.wikipedia.org/wiki/SHA-2
+        /// </summary>
+        SHA2_384,
+
+        /// <summary>
+        ///     SHA-2 512 Bit
+        ///     https://en.wikipedia.org/wiki/SHA-2
+        /// </summary>
+        SHA2_512,
+
+        /// <summary>
+        ///     SHA-3 256 Bit
+        ///     https://en.wikipedia.org/wiki/SHA-3
+        /// </summary>
+        SHA3_256,
+
+        /// <summary>
+        ///     SHA-3 384 Bit
+        ///     https://en.wikipedia.org/wiki/SHA-3
+        /// </summary>
+        SHA3_384,
+
+        /// <summary>
+        ///     SHA-3 512 Bit
+        ///     https://en.wikipedia.org/wiki/SHA-3
+        /// </summary>
+        SHA3_512,
+
+        /// <summary>
+        ///     RIPEMD 128 Bit
+        ///     https://en.wikipedia.org/wiki/RIPEMD
+        /// </summary>
+        RipeMD_128,
+
+        /// <summary>
+        ///     RIPEMD 160 Bit
+        ///     https://en.wikipedia.org/wiki/RIPEMD
+        /// </summary>
+        RipeMD_160,
+
+        /// <summary>
+        ///     RIPEMD 256 Bit
+        ///     https://en.wikipedia.org/wiki/RIPEMD
+        /// </summary>
+        RipeMD_256,
+
+        /// <summary>
+        ///     RIPEMD 320 Bit
+        ///     https://en.wikipedia.org/wiki/RIPEMD
+        /// </summary>
+        RipeMD_320,
+
+        /// <summary>
+        ///     Whirlpool 512 Bit
+        ///     https://en.wikipedia.org/wiki/Whirlpool_(hash_function)
+        /// </summary>
+        Whirlpool
+
+    }
+}
---
+++ StringEncoding.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CryptoShark
+{
+    /// <summary>
+    ///     String Encoding Options
+    /// </summary>
+    public enum StringEncoding
+    {
+        /// <summary>
+        ///     Hexadecimal format
+        /// </summary>
+        Hex,
+
+        /// <summary>
+        ///     Base64 Encoded
+        /// </summary>
+        Base64,
+
+        /// <summary>
+        ///     Url Base64 Encoded
+        ///     (safe for use as part of a url query string)
+        /// </summary>
+        UrlBase64
+    }
+}
